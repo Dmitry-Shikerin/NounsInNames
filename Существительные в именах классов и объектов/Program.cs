ce2c269234c0bdb2eb492fd80dca5cf209a31adc
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-06 12:44:36 +0300

    Добавьте файлы проекта.

---
+++ Существительные в именах классов и объектов/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Существительные_в_именах_классов_и_объектов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class PlayerData { }
+    class GunController { }
+    class FollowTarget { }
+    class ReadingUnit
+    {
+        public IReadOnlyCollection<Unit> Units { get; private set; }
+    }
+
+    class Unit { }
+}
